dd5192d536dc130095cbe420d66c15cc9b0035d6
Hoang Le <le.th.hoang98@gmail.com>
2025-03-12 00:34:04 +0200

    signal to client

--- client.c
+++ client.c
@@ -1,7 +1,32 @@
 
 #include "minitalk.h"
 
-void	signal_action(pid_t pid, char c)
+static volatile int	g_msg_received = 0;
+
+static void	handle_server_signal(int sig, siginfo_t *info, void *context)
+{
+	static int	bit = 0;
+	static pid_t	server_pid;
+
+	(void)context;
+	if (info->si_pid != 0)
+		server_pid = info->si_pid;
+	if (sig == SIGUSR2)
+	{
+		bit++ ;
+		g_msg_received = 1;
+	}
+	else if (sig == SIGUSR1)
+	{
+		g_msg_received = 1;
+		usleep(400);
+		ft_printf("Server (PID: %d) received %d bytes", server_pid, bit / 8);
+		ft_printf("(%d bits) and printed the full msg.\n", bit);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void	send_char(pid_t pid, char c)
 {
 	int	bit;
 
@@ -12,26 +37,42 @@ void	signal_action(pid_t pid, char c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
+		while (g_msg_received == 0)
+			pause();
+		g_msg_received = 0;
 		bit++ ;
-		usleep(500);
 	}
+}
+
+static void	send_msg(int pid, char *msg)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(msg))
+	{
+		send_char(pid, msg[i]);
+		i++ ;
+	}
+	send_char(pid, '\0');
+	while (g_msg_received == 0)
+		pause();
 }
 
 int	main(int ac, char ** av)
 {
 	pid_t	pid;
+	char	*msg;
 	int		i;
 
 	i = 0;
 	if (ac == 3)
 	{
+		signal(SIGUSR1, handle_server_signal);
+		signal(SIGUSR2, handle_server_signal);
 		pid = ft_atoi(av[1]);
-		while (av[2][i])
-		{
-			signal_action(pid, av[2][i]);
-			i++ ;
-		}
-		signal_action(pid, '\n');
+		msg = av[2];
+		send_msg(pid, msg);
 	}
 	else
 		return(ft_printf("Wrong arguments\n"), EXIT_FAILURE);
--- server.c
+++ server.c
@@ -1,17 +1,38 @@
 
 #include "minitalk.h"
 
-void	signal_handler(int signal)
+char	g_received_msg[2000000];
+
+static int	end_of_msg(pid_t client_pid, int char_index)
+{
+	ft_printf("Received message:\n%s\n", g_received_msg);
+	ft_bzero(g_received_msg, 2000000);
+	kill(client_pid, SIGUSR1);
+	return (char_index = 0);
+}
+
+static void	signal_handler(int signal, siginfo_t *info, void *context)
 {
 	static int	bit = 0;
-	static int	c = 0;
+	static char	c = 0;
+	static int	i = 0;
+	static pid_t	client_pid;
 
-	if (signal == SIGUSR1)
-		c |= (0x01 << bit);
+	(void)context;
+	c <<= 1;
+	if (info->si_pid != 0)
+		client_pid = info->si_pid;
+	if (signal == SIGUSR2)
+		c |= 1;
+	if (signal == SIGUSR2 || signal == SIGUSR2)
+		kill(client_pid, SIGUSR2);
 	bit++ ;
 	if (bit == 8)
 	{
-		ft_printf("%c", c);
+		if (c == '\0')
+			i = end_of_msg(client_pid, i);
+		else
+			g_received_msg[i++] = c;
 		bit = 0;
 		c = 0;
 	}
@@ -31,6 +52,7 @@ int	main(int ac, char **av)
 		return(ft_printf("Error SIGUSR1 signal handler"), 1);
 	if (signal(SIGUSR2, signal_handler) == SIG_ERR)
 		return(ft_printf("Error SIGUSR2 signal handler"), 1);
+	ft_bzero(g_received_msg, 2000000);
 	while (1)
 		pause();
 	return (EXIT_SUCCESS);
